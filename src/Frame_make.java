1736c4b0276b8094d339c043cff2ca7abb42f8a4
JeesooHa <jsha2913@gmail.com>
2016-11-11 14:30:20 +0900

    delete comment

--- src/Frame_make.java
+++ src/Frame_make.java
@@ -11,35 +11,6 @@ public class Frame_make {
 }
 
 class Frame_make_sub extends JFrame{ 
-<<<<<<< HEAD
-
-	int f_width = 800; 
-	int f_height = 600;
-	
-	Frame_make_sub(){ 
-	
-		init();
-		start(); 
-	
-		setTitle("Frame Test");
-		setSize(f_width, f_height); 
-		
-		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		
-		int f_xpos = (int)(screen.getWidth() / 2 - f_width / 2);
-		int f_ypos = (int)(screen.getHeight() / 2 - f_height / 2);
-		
-		setLocation(f_xpos, f_ypos);	//set frame location 
-		
-		setResizable(false);
-		setVisible(true); 
-	}
-
-	public void init(){ 
-	}
-	
-	public void start(){
-=======
 	
 	int f_width = 800; 
 	int f_height = 600; 
@@ -71,6 +42,5 @@ class Frame_make_sub extends JFrame{
 	
 	public void start(){ 
 
->>>>>>> 98e5fa297460f10b91b24651aa4b107d37799ea6
 	}
 }
